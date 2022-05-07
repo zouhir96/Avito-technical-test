980c9dacd02da863fcf41cd256f87f846b77ed40
zouhir <rajdaouizouhir.pro@gmail.com>
2022-05-07 03:44:52 +0000

    initial commit

---
+++ app/src/main/java/com/zrcoding/android_challenge/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.zrcoding.android_challenge.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zrcoding.android_challenge.R
+import com.zrcoding.android_challenge.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
